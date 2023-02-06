1aa487dc2df8745a00bc3aff1dfa357a72e75034
Maheswaran M <Maheswaran.Muthuswamy@tatacommunications.com>
2023-02-06 17:17:56 +0530

    New scriipt addes

---
+++ sysmon.py
@@ -0,0 +1,13 @@
+import psutil
+import time
+
+while True:
+    cpu_percent = psutil.cpu_percent()
+    virtual_memory = psutil.virtual_memory()
+    disk_usage = psutil.disk_usage("/")
+
+    print("CPU Usage: ", cpu_percent, "%")
+    print("Memory Usage: ", virtual_memory.percent, "%")
+    print("Disk Usage: ", disk_usage.percent, "%")
+
+    time.sleep(5)
